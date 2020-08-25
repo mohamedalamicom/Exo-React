a232cf9ba68364dc485d15627a118cf87aa5fbdb
spb-malami <49144201+spb-malami@users.noreply.github.com>
2020-08-25 22:12:30 +0200

    /countries : GET all country values.

--- Client/app.js
+++ Client/app.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+
+app.get('/countries/:keywords?', (req, res, next) => {
+    // Retrieving keywords (optional) 
+    let searchKeywords = req.params.keywords;
+
+    // Reading from countries list JSON
+    let data = fs.readFileSync('./countries.json');
+    let countries = JSON.parse(data);
+
+    if (searchKeywords) {
+        let results = [];
+
+        countries.forEach(country => {
+            if (country.name.toUpperCase().includes(searchKeywords.toUpperCase()))
+                results.push(country);
+        })
+
+        res.send(results);
+    }
+
+    res.send(countries);
+})
 
 app.get('/', (req, res) => {
     res.send('Exo-React\'s server is running');
