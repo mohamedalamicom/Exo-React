25044db05da66b2fa9c7d1e2dfd1d00d82a3f6f1
spb-malami <49144201+spb-malami@users.noreply.github.com>
2020-08-25 22:15:57 +0200

    /customer : POST create  customer.

--- Client/app.js
+++ Client/app.js
@@ -1,7 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const fs = require('fs')
+const { body, validationResult } = require('express-validator')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/customer', [
+    // Validation with Express-Validator
+
+    // First step
+    body('firstname').isAlpha(),
+    body('lastname').isAlpha(),
+    body('title').isAlpha(),
+    body('email').isEmail(),
+    body('mobile').isMobilePhone(),
+
+    // Second step
+    body('country').isLength({ min: 3, max: 46 }),
+    body('street').isLength({ min: 3, max: 255 }),
+    body('city').isLength({ min: 3, max: 28 }),
+
+    // Third step
+    // Bank informations ?
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.sendStatus(200)
+})
 
 app.get('/countries/:keywords?', (req, res, next) => {
     // Retrieving keywords (optional) 
